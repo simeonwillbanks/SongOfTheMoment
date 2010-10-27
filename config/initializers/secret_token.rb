7fe42b2a30f4ab0408812f6cf11b6895c434fcee
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2010-10-27 15:54:37 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Songofthemoment::Application.config.secret_token = 'b6380b2a65ed654a91238b22b9dd50077e53723fa66db11e399fb17ad367f2cddca5b5508383f21c1603b538632e15e6be643b11a4d2e4d7a5afe5dcf527f085'
