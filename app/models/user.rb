e8b47c242015132a63e0452476c1f48f214db525
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2010-11-03 08:29:08 -0700

    Add encrypted password and salt to users table.  Salt is unique for each user, and its set from UTC. The password is encrypted with SHA2.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,18 @@
 # == Schema Information
-# Schema version: 20101028150100
+# Schema version: 20101103151923
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
 #
+
+require 'digest'
 
 class User < ActiveRecord::Base
   attr_accessor :password
@@ -28,14 +32,30 @@ class User < ActiveRecord::Base
                        
   before_save :encrypt_password
   
+  # Return true if the user's password matches the submitted password
+  def has_password?(submitted_password)
+    # Compare encrypted_password with the encrypted version of 
+    # submitted_password
+    encrypted_password == encrypt(submitted_password)
+  end
+  
   private
   
     def encrypt_password
+      self.salt = make_salt if new_record?
       self.encrypted_password = encrypt(password)
     end
     
     def encrypt(string)
-      string
+      secure_hash("#{salt}--#{string}")
+    end
+    
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+    
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
     end
   
 end
