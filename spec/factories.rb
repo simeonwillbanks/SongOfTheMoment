844fb5fe55422d2c97bca626a505e2e797a222c7
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2010-11-09 08:34:04 -0800

    Add Gravatar gem for user avatars, and add User helper to render Gravatars. Add tests for new user show template. Sync User factory with first user in development database for tests to pass.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 # By using the symbol ':user', we get Factory Girl to simulate the User model
 Factory.define :user do |user|
-  user.name                  "Simeon Willbanks"
-  user.email                 "simeon@example.com"
+  user.name                  "Example User"
+  user.email                 "example@railstutorial.org"
   user.password              "foobar"
   user.password_confirmation "foobar"
 end
