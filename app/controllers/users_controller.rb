f59387530d02897e493007992df7ad8544d028f0
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2010-11-10 09:19:28 -0800

    Add UsersController.create and relevant tests to power a user signup form. So far, only validations are working.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,18 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
     @title = "Join"
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # Handle a successful save.
+    else
+      @title = "Join"
+      render 'new'
+    end
   end
 
 end
