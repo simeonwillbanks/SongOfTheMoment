311745c6a12b00527d09a0fdc0614677cd3e8b73
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2010-11-10 09:18:25 -0800

    Add ApplicationHelper.mark_required. The method returns a string containing an asterisk if the model’s validators include a PresenceValidator.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,10 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
+  
+  # Returns a string containing an asterisk if the model’s validators 
+  # include a PresenceValidator
+  def mark_required(object, attribute)  
+    "*" if object.class.validators_on(attribute).map(&:class).include? ActiveModel::Validations::PresenceValidator  
+  end
 end
